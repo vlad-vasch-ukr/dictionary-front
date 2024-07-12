c59417635575c90bff32356143940af186e7338b
Vladyslav <vashchenko-v@lxuser-HP-ProBook-450-15-6-inch-G10-Notebook-PC>
2024-07-12 15:38:08 +0200

    add register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RegisterFormComponent} from './features/authorization/register-form/register-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    ReactiveFormsModule,
-    RegisterFormComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
